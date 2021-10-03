3dde67e50cb244095656d47ba0d215965bc7ca4f
Chandra Katragadda <19722118+cpkatragadda@users.noreply.github.com>
2021-10-03 01:18:36 -0400

    removed debug print statements and fixed formatting issues

--- acm-cert-expiry-lambda.py
+++ acm-cert-expiry-lambda.py
@@ -16,9 +16,10 @@ logger.setLevel(logging.INFO)
 utc = timezone.utc
 
 # make today timezone aware
-today = datetime.now().replace(tzinfo=utc)
+today = datetime.now().replace(tzinfo=timezone.utc)
 
 # set up time window for alert - default to 90 if its missing
+
 if os.environ.get('EXPIRY_DAYS') is None:
     logger.info('No value found for EXPIRY_DAYS from Environment, Using default 90 days')
     expiry_days = 90
@@ -29,19 +30,18 @@ expiry_window = today + timedelta(days = expiry_days)
 
 
 def lambda_handler(event, context):
-    print('in lambda handler')
     response = handle_multiple_certs(event, context)
     
+    # For debugging
+    print(response)
     return {
         'statusCode': 200,
         'body': response 
     }
-    print('return')
 
 
 def handle_multiple_certs(event, context_arn):
-    cert_client = boto3.client('acm')
-    print('in handle multiple certs')
+    cert_client = boto3.client('acm', region_name='us-east-1')
     cert_list = json.loads(get_expiring_cert_arns())
     
     if cert_list is None:
@@ -51,20 +51,20 @@ def handle_multiple_certs(event, context_arn):
         response = 'The following certificates are expiring within ' + str(expiry_days) + ' days: \n'
 
         # loop through the cert list and pull out certs that are expiring within the expiry window
-        for csl in cert_list:
-            cert_arn = json.dumps(csl['Dimensions'][0]['Value']).replace('\"', '')
+        for cert in cert_list:
+            cert_arn = json.dumps(cert['Dimensions'][0]['Value']).replace('\"', '')
             cert_details = cert_client.describe_certificate(CertificateArn=cert_arn)
 
             if cert_details['Certificate']['NotAfter'] < expiry_window:
-                current_cert = 'Domain:' + cert_details['Certificate']['DomainName'] + ' (' + cert_details['Certificate']['CertificateArn'] + '), \n'
-
+                certificateExpiresIn = cert_details['Certificate']['NotAfter'].replace(tzinfo=None) - today.replace(tzinfo=None)
+                current_cert = 'Domain:' + cert_details['Certificate']['DomainName'] + ' (' + cert_details['Certificate']['CertificateArn'] + '), ' + 'Expires in' + str(certificateExpiresIn.days) + ' days ' "\n"
 
                 # This is the text going into the SNS notification
                 response = response + current_cert
                 
     # if there's an SNS topic, publish a notification to it
     if os.environ.get('SNS_TOPIC_ARN') is not None:
-        sns_client = boto3.client('sns')
+        sns_client = boto3.client('sns', region_name='us-east-1')
         response = sns_client.publish(TopicArn=os.environ['SNS_TOPIC_ARN'], Message=response.rstrip(', \n'), Subject='Certificate Expiration Notification')
 
     return response
@@ -86,6 +86,9 @@ def get_expiring_cert_arns():
 
     # return all certs that are expiring according to CW
     return json.dumps(cert_list)
+
+# Code block to invoke lambda_handler to test in local mode
+    
 event = []
 context = [{'invoked_function_arn': 'test'}]
 lambda_handler(event, context)
